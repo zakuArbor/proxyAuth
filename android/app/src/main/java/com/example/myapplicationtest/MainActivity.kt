b87e82e9a2c2def209365a2f18d659ab703d89cb
anurag bist <anura@192.168.0.19>
2020-02-12 00:00:45 -0500

    got the branch working

--- android/app/src/main/java/com/example/myapplicationtest/MainActivity.kt
+++ android/app/src/main/java/com/example/myapplicationtest/MainActivity.kt
@@ -24,16 +24,10 @@ class MainActivity : AppCompatActivity() {
 
     var mDeviceList = ArrayList<String>()
 
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_main)
-        /*
-        list_view.setOnClickListener { /*parent, view, position, id ->
-
-
-         */
-
-
 
         val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
         if (bluetoothAdapter == null) {
@@ -50,15 +44,15 @@ class MainActivity : AppCompatActivity() {
             val deviceName = device.name
             val deviceHardwareAddress = device.address // MAC address
             mDeviceList.add(device.name + "\n" + device.address)
-            Log.i("BTBTBT", device.name + "\n" + device.address + "---------------------------PP")
-            list_view.adapter = ArrayAdapter<String>(applicationContext,
-                android.R.layout.simple_list_item_1, mDeviceList)
-            /*{
-                @Override
-                fun V
-            }
+            Log.i(
+                "BTBTBT",
+                device.name + "\n" + device.address + "---------------------------PP"
+            )
+            list_view.adapter = ArrayAdapter<String>(
+                applicationContext,
+                android.R.layout.simple_list_item_1, mDeviceList
+            )
 
-             */
         }
 
         // Register for broadcasts when a device is discovered.
@@ -67,40 +61,13 @@ class MainActivity : AppCompatActivity() {
 
         Log.i("BTBTBT", "made it here ----------------------------")
 
-        //val discoverableIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
-        //    putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 60)
-        //}
-
-
-        //val context: Context = MainActivity.
-
-        //receiver.onReceive(applicationContext,discoverableIntent)
-        //startActivity(discoverableIntent)
 
         list_view.setOnItemClickListener { adapterView: AdapterView<*>?,
                                            view: View?, position: Int, id: Long ->
-            Log.i("BTBTBT","BT_Device-------------------" + mDeviceList[position])
+            Log.i("BTBTBT", "BT_Device-------------------" + mDeviceList[position])
         }
         button_change.setOnClickListener {
             TB.text = "new text"
-            /*
-            val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
-            if (bluetoothAdapter == null) {
-                // Device doesn't support Bluetooth
-            }
-
-            if (bluetoothAdapter?.isEnabled == false) {
-                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                startActivityForResult(enableBtIntent, 1)
-            }
-
-            val discoverableIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
-                putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 60)
-            }
-
-            startActivity(discoverableIntent)
-
-             */
         }
     }
 
@@ -115,7 +82,7 @@ class MainActivity : AppCompatActivity() {
         override fun onReceive(context: Context, intent: Intent) {
             val action: String = intent.action
             Log.i("BTBTBT", "called here ----------------------------")
-            when(action) {
+            when (action) {
                 BluetoothDevice.ACTION_FOUND -> {
                     // Discovery has found a device. Get the BluetoothDevice
                     // object and its info from the Intent.
@@ -126,70 +93,19 @@ class MainActivity : AppCompatActivity() {
                     mDeviceList.add(device.name + "\n" + device.address)
 
 
-                    Log.i("BTBTBT", device.name + "\n" + device.address + "---------------------------DD")
+                    Log.i(
+                        "BTBTBT",
+                        device.name + "\n" + device.address + "---------------------------DD"
+                    )
 
-                    list_view.adapter = ArrayAdapter<String>(context,
-                        android.R.layout.simple_list_item_1, mDeviceList)
+                    list_view.adapter = ArrayAdapter<String>(
+                        context,
+                        android.R.layout.simple_list_item_1, mDeviceList
+                    )
 
 
                 }
             }
         }
     }
-
-
-    /*
-    private inner class ConnectThread(device: BluetoothDevice) : Thread() {
-
-        private val mmSocket: BluetoothSocket? by lazy(LazyThreadSafetyMode.NONE) {
-            device.createRfcommSocketToServiceRecord(0)
-        }
-
-        public override fun run() {
-
-            val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
-            if (bluetoothAdapter == null) {
-                // Device doesn't support Bluetooth
-            }
-
-            if (bluetoothAdapter?.isEnabled == false) {
-                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                startActivityForResult(enableBtIntent, 1)
-            }
-
-            val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
-            pairedDevices?.forEach { device ->
-                val deviceName = device.name
-                val deviceHardwareAddress = device.address // MAC address
-            }
-
-            // Cancel discovery because it otherwise slows down the connection.
-            bluetoothAdapter?.cancelDiscovery()
-
-            mmSocket?.use { socket ->
-                // Connect to the remote device through the socket. This call blocks
-                // until it succeeds or throws an exception.
-                socket.connect()
-
-                // The connection attempt succeeded. Perform work associated with
-                // the connection in a separate thread.
-                //manageMyConnectedSocket(socket)
-            }
-        }
-
-        // Closes the client socket and causes the thread to finish.
-        fun cancel() {
-            try {
-                mmSocket?.close()
-            } catch (e: IOException) {
-                Log.e(TAG, "Could not close the client socket", e)
-            }
-        }
-    }
-    */
-
-
-
-
-
 }
