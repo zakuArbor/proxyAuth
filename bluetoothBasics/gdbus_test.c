e7bde9a90e970d38161b11811f38cf97fe4c8e8e
Ju Hong Kim <nabimoon1234@gmail.com>
2020-03-02 00:11:47 -0500

    Use Dictionary lookup to get needed info

--- bluetoothBasics/gdbus_test.c
+++ bluetoothBasics/gdbus_test.c
@@ -3,20 +3,6 @@
 #include <glib.h>
 #include <gio/gio.h>
 
-#define DEVICE 20:DA:22:DE:0F:68
-
-static void bluez_property_value(const gchar *key, GVariant *value)
-{
-	const gchar *type = g_variant_get_type_string(value);
-	if (!g_ascii_strcasecmp("Address", key)) {
-		g_print("\t\t%s: %s\n", key, g_variant_get_string(value, NULL));
-	}
-	if (!g_ascii_strcasecmp("Connected", key) && g_variant_get_boolean(value) == 1) {
-		g_print("\t\t%s (%c) : %d\n", key, *type, g_variant_get_boolean(value));
-	}
-
-}
-
 int main() {
 	GDBusConnection *conn;
 
@@ -55,14 +41,14 @@ int main() {
 			g_variant_iter_init(&ii, ifaces_and_properties);
 			while(g_variant_iter_next(&ii, "{&s@a{sv}}", &interface_name, &properties)) {
 				if(g_strstr_len(g_ascii_strdown(interface_name, -1), -1, "device")) {
-					g_print("[ %s ]\n", object_path);
- 					const gchar *property_name;
-					GVariantIter iii;
-					GVariant *prop_val;
-					g_variant_iter_init(&iii, properties);
-					while(g_variant_iter_next(&iii, "{&sv}", &property_name, &prop_val))
-						bluez_property_value(property_name, prop_val);
-					g_variant_unref(prop_val);
+					GVariantDict prop_dict;
+					g_variant_dict_init(&prop_dict, properties);
+					GVariant *is_connected_gvariant = g_variant_dict_lookup_value(&prop_dict, "Connected", G_VARIANT_TYPE("b"));
+					if (g_variant_get_boolean(is_connected_gvariant)) {
+						g_print("[ %s ]\n", object_path);
+						GVariant *addr_gvariant = g_variant_dict_lookup_value(&prop_dict, "Address", G_VARIANT_TYPE("s"));
+						g_print("pika: %s\n", g_variant_get_string(addr_gvariant, NULL));
+					}
 				}
 				g_variant_unref(properties);
 			}
