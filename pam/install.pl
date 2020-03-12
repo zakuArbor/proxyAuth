a366d57be13fbaf2965f123ad45b2e83ab768bff
Ju Hong Kim <nabimoon1234@gmail.com>
2020-03-12 00:11:07 -0400

    Updated Installation Script
    Changed way to detect dependencies and have installation be done without prompting. Since I ran the update commands with bacticks, no output is being shown during installtion process so I should consider using pipes

--- pam/install.pl
+++ pam/install.pl
@@ -5,32 +5,33 @@ use warnings;
 
 sub check_dependency {
 	my ($dep) = @_;
-	my $cmd = "ldconfig -p | grep $dep | wc -l";
-    return `$cmd`;
+	my $cmd = "dpkg -s $dep 2>&1 1>/dev/null";
+    `$cmd`;
+    return not $?;
 }
 
 {
-	my $install_manager_cmd = "sudo apt-get install";
+	my $install_manager_cmd = "sudo apt-get -y install";
 
-	my %dependencies = (
-		'libpam' => 'libpam0g-dev',
-		'libglib' => 'libglib2.0-dev',
-		'libbluetooth' => 'bluez'
-	);
+	my @dependencies = ('libpam0g-dev', 'libglib2.0-dev', 'bluez', 'libbluetooth-dev');
 
 	my @directories = ('/lib/security/', '/etc/proxy_auth/');
 
 	#install missing dependencies
-	for my $dep (keys %dependencies) {
+	for my $dep (@dependencies) {
+		print "Checking if $dep is installed\n";
 		if (check_dependency($dep) == 0) {
-			`$install_manager_cmd $dependencies{$dep}`
+			print "$dep not installed\n";
+			print "Installing $dep\n";
+			print "$install_manager_cmd $dep\n";
+			`$install_manager_cmd $dep`;
 		}
 	}
-
+	
 	#create missing directories
 	for my $dir (@directories) {
 		if (not -d $dir) {
-			`sudo mkdir $dir` 
+			`sudo mkdir $dir`;
 		}
 	}
 }
