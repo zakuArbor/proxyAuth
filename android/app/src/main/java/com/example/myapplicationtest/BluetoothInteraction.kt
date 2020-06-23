bd105deff08832d0dac20b34ae3e0e0c194efa75
Ju Hong Kim <nabimoon1234@gmail.com>
2020-06-23 05:16:41 -0400

    [#42] Disconnect from server if server connection is closed

--- android/app/src/main/java/com/example/myapplicationtest/BluetoothInteraction.kt
+++ android/app/src/main/java/com/example/myapplicationtest/BluetoothInteraction.kt
@@ -1,8 +1,6 @@
 package com.example.myapplicationtest
 
-import android.app.Activity
 import android.app.IntentService
-import android.app.ProgressDialog
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
@@ -14,19 +12,17 @@ import java.util.*
 
 class BluetoothInteraction : IntentService("BluetoothInteraction") {
     companion object {
-        var m_myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
-        var m_bluetoothSocket: BluetoothSocket? = null
-        lateinit var m__bluetoothAdapter: BluetoothAdapter
-        var m_isConnected: Boolean = false
-        lateinit var m_address: String
-        lateinit var m_name: String
-        private val mmBuffer: ByteArray = ByteArray(1024) // mmBuffer store for the stream
+        var my_uuid: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+        var bluetooth_socket: BluetoothSocket? = null
+        lateinit var bluetooth_adapter: BluetoothAdapter
+        lateinit var server_address: String
+        lateinit var server_name: String
+        private val buffer: ByteArray = ByteArray(1024) // mmBuffer store for the stream
     }
 
     override fun onHandleIntent(intent: Intent) {
-        // Gets data from the incoming Intent
-        m_address = intent.getStringExtra(MainActivity.EXTRA_ADDRESS)
-        m_name = intent.getStringExtra(MainActivity.DEVICE_NAME)
+        server_address = intent.getStringExtra(MainActivity.EXTRA_ADDRESS)
+        server_name = intent.getStringExtra(MainActivity.DEVICE_NAME)
 
         // Do work here, based on the contents of dataString
         if (!connect()) {
@@ -35,57 +31,44 @@ class BluetoothInteraction : IntentService("BluetoothInteraction") {
         runServer()
     }
 
-    private fun sendCommand(input: String){
-        if (m_bluetoothSocket != null){
-            try{
-                //Log.d("data", "DATA Incoming")
-                Log.d("data", input)
-                m_bluetoothSocket!!.outputStream.write(input.toByteArray())
-            } catch (e: IOException){
-                e.printStackTrace()
-            }
-        }
+    override fun onDestroy() {
+        disconnect()
     }
 
     private fun connect() : Boolean {
         try{
-            if(m_bluetoothSocket == null || !m_isConnected){
-                m__bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-                val device: BluetoothDevice = m__bluetoothAdapter.getRemoteDevice(
-                    m_address
-                )
-                m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(
-                    m_myUUID
-                )
-                //stops the app for looking for other devices
+            if(bluetooth_socket == null || !bluetooth_socket!!.isConnected){
+                Log.d("data", "stage 1\n")
+                bluetooth_adapter = BluetoothAdapter.getDefaultAdapter()
+                Log.d("data", "stage 2\n")
+                val device: BluetoothDevice = bluetooth_adapter.getRemoteDevice(server_address)
+                Log.d("data", "stage 3\n")
+                bluetooth_socket = device.createRfcommSocketToServiceRecord(my_uuid)
+                Log.d("data", "stage 4\n")
+                //stop discovery mode to stop devices attempting to connect to the app
                 BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
-                m_bluetoothSocket!!.connect()
-                Log.i("data", "Successfully Connected")
-                m_isConnected = true
+                Log.d("data", "stage 5\n")
+                bluetooth_socket!!.connect()
+                Log.d("data", "Successfully Connected")
                 return true
             }
         }catch (e: IOException){
             Log.d("data", "Failed to Connect \n")
             e.printStackTrace()
+            disconnect()
         }
         return false
     }
 
-    override fun onDestroy() {
-        disconnect()
-    }
-
     private fun disconnect(){
-        if (m_bluetoothSocket != null) {
+        if (bluetooth_socket != null) {
             try {
-                m_isConnected = false
-                m_bluetoothSocket!!.outputStream.close()
-                m_bluetoothSocket!!.inputStream.close()
-                m_bluetoothSocket!!.close()
+                bluetooth_socket!!.outputStream.close()
+                bluetooth_socket!!.inputStream.close()
+                bluetooth_socket!!.close()
             } catch (e: IOException) {
-                e.printStackTrace()
             } finally { // close the socket
-                m_bluetoothSocket = null
+                bluetooth_socket = null
                 Toast.makeText(this, "Disconnecting from Server", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, MainActivity::class.java)
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -94,34 +77,38 @@ class BluetoothInteraction : IntentService("BluetoothInteraction") {
         }
     }
 
-    private fun runServer() {
-        var buf : String
+    private fun sendCommand(input: String){
+        if (bluetooth_socket == null || !bluetooth_socket!!.isConnected) {
+            disconnect()
+        }
 
-        while(m_isConnected) {
-            buf = receiveCommand()
-            if (buf.length < 0) {
-                Log.d("data", "Failed to receive data")
-                stopSelf()
-                //break
-            }
-            sendCommand("World")
+        try{
+            bluetooth_socket!!.outputStream?.write(input.toByteArray())
+        } catch (e: IOException){
         }
     }
 
     private fun receiveCommand() : String {
-        var numBytes: Int // bytes returned from read()
-        try {
-            // Keep listening to the InputStream until an exception occurs.
-            // Read from the InputStream.
-            numBytes = m_bluetoothSocket!!.inputStream.read(mmBuffer)
-            Log.d("data", numBytes.toString())
-            Log.d("data", mmBuffer.toString(Charsets.UTF_8))
-            Toast.makeText(this, mmBuffer.toString(Charsets.UTF_8), Toast.LENGTH_LONG)
-            return mmBuffer.toString(Charsets.UTF_8)
-        } catch (e: IOException) {
-            Log.d("data", "Input stream was disconnected", e)
+        if (bluetooth_socket == null || !bluetooth_socket!!.isConnected) {
+            disconnect()
         }
-        return ""
+
+        bluetooth_socket!!.inputStream.read(buffer)
+        Log.d("data", buffer.toString(Charsets.UTF_8))
+        return buffer.toString(Charsets.UTF_8)
+    }
+
+    private fun runServer() {
+        var buf : String
+
+        while(bluetooth_socket != null && bluetooth_socket!!.isConnected) {
+            buf = receiveCommand()
+            if (buf == "" || buf.length < 0) {
+                Log.d("data", "Failed to receive data")
+                stopSelf()
+            }
+            sendCommand("World")
+        }
+        disconnect()
     }
 }
-
